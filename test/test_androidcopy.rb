369f676f8ffccd0ab817c3d4f212021aea2adb6c
Trujun Zhang <trujunzhang@gmail.com>
2016-03-17 16:12:13 +0800

    Android Copy fn.

--- test/test_androidcopy.rb
+++ test/test_androidcopy.rb
@@ -1,9 +1,19 @@
 require 'test/unit'
 require 'Androidcopy'
+require 'android_resource_util'
 
 class AndroidcopyTest < Test::Unit::TestCase
   def test_any_hello
     message = Androidcopy.hi()
     assert_equal "world!", message
+  end
+
+  def test_source_path_list
+    source_path = "/Users/djzhang/Desktop/wikipedia/apps-android-wikipedia/app/src/main/res"
+    dest_Path = "/tmp/IEATTA-ANDROID/res"
+    fileName = "ic_search.png"
+    files = AndroidResourceUtil.getAllFiles(source_path, fileName)
+
+    assert_equal files.length,10
   end
 end
