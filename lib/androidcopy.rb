f33393a615e22c8869dc31c3575baacf393e729e
Trujun Zhang <trujunzhang@gmail.com>
2016-03-17 16:50:00 +0800

    Android Copy fn.

--- lib/androidcopy.rb
+++ lib/androidcopy.rb
@@ -11,6 +11,7 @@ module Androidcopy
 
   def self.copy(source_path, target_path, fileName)
     files = AndroidResourceUtil.getAllFiles(source_path, fileName)
+    puts "Count: #{files.length}"
     files.each { |i|
       source = "#{i}/#{fileName}"
       foldName = File.basename("#{i}")
@@ -18,23 +19,14 @@ module Androidcopy
       target_fold = "#{target_path}/#{foldName}"
       target = "#{target_fold}/#{fileName}"
       FileUtils.makedirs("#{target_fold}")
+
+      puts "Copy"
+      puts "from #{source}"
+      puts "to #{target}"
       FileUtils.copy source, target
     }
 
   end
-
-  def copy_without_svn(source_path, target_path)
-    Find.find(source_path) do |source|
-      target = source.sub(/^#{source_path}/, target_path)
-      if File.directory? source
-        Find.prune if File.basename(source) == '.svn'
-        FileUtils.mkdir target unless File.exists? target
-      else
-        FileUtils.copy source, target
-      end
-    end
-  end
-
 end
 
 
